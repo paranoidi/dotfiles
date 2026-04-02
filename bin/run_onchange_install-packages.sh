aca34726a35608ae782ebfff16783c3af380ceba
paranoidi <marko.koivusalo@gmail.com>
2026-04-02 12:07:10 +0300

    Install improvements

--- bin/run_onchange_install-packages.sh
+++ bin/run_onchange_install-packages.sh
@@ -3,6 +3,14 @@
 # Check and install packages only if they're not already installed
 packages_to_install=()
 packages=(curl wget git mc task-spooler tmux git-delta fish fd-find bat neovim gh jq unzip)
+gui_packages=(phinger-cursor-theme)
+
+# Add GUI packages to install list
+if [[ -n "$DISPLAY" || -n "$WAYLAND_DISPLAY" ]] || \
+   pgrep -x "Xorg" >/dev/null || \
+   pgrep -x "wayland" >/dev/null; then
+    packages+=("${gui_packages[@]}")
+fi
 
 # Remove packages not available on Debian 11
 if [ -f /etc/issue ] && grep -q "Debian GNU/Linux 11" /etc/issue; then
@@ -67,7 +75,7 @@ if command -v starship >/dev/null 2>&1; then
 else
     echo "🚀 Installing starship..."
     set +e  # Temporarily disable exit on error
-    curl -sS https://starship.rs/install.sh | sh
+    curl -sS https://starship.rs/install.sh -y | sh
     starship_exit_code=$?
     set -e  # Re-enable exit on error
     if [ $starship_exit_code -eq 0 ]; then
