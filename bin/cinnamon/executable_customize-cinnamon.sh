2f1cdaca1318ca73ab9e91e29c84d22aafcea2c9
paranoidi <marko.koivusalo@gmail.com>
2026-04-15 20:41:02 +0300

    Added cinnamon customization script

---
+++ bin/cinnamon/executable_customize-cinnamon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# How to open new windows
+gsettings set org.cinnamon.muffin placement-mode "center"
+
+# set screensaver to 2 minutes
+gsettings set org.cinnamon.desktop.session idle-delay 300
+
+# switch capslock to esc
+#setxkbmap -option caps:escape
+
