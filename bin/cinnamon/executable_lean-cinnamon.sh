60b10349cd6dc47e291e39cb31dd0d685651f6a0
paranoidi <marko.koivusalo@gmail.com>
2026-04-22 14:04:21 +0300

    Update executable_lean-cinnamon.sh
    Remove more crud

--- bin/cinnamon/executable_lean-cinnamon.sh
+++ bin/cinnamon/executable_lean-cinnamon.sh
@@ -1,5 +1,5 @@
 # Define the package list (edit this line as needed)
-packages="warpinator celluloid hypnotix matrix rhythmbox transmission thunderbird drawing simple-scan"
+packages="warpinator celluloid hypnotix matrix rhythmbox transmission transmission-common thunderbird drawing simple-scan"
 
 # Filter only installed packages
 installed=$(dpkg-query -W -f='${binary:Package}\n' $packages 2>/dev/null)
