7bce5804b5c73242d3db217d5cbba42f307a66be
paranoidi <marko.koivusalo@gmail.com>
2026-05-01 16:31:53 +0300

    Deprecated tmux helper in favour of tt

--- dot_config/fish/functions/tmux.fish
+++
@@ -1,7 +0,0 @@
-function tmux --description 'Run tmux, default to main session'
-    if test (count $argv) -eq 0
-        command tmux new-session -A -s main
-    else
-        command tmux $argv
-    end
-end
