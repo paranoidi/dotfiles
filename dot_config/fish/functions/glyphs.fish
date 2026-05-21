539d922789415aad511bcf6b90f622a34cff01fe
paranoidi <marko.koivusalo@gmail.com>
2026-05-22 02:30:18 +0300

    feat: glyphs helper now shows \uXXX code

--- dot_config/fish/functions/glyphs.fish
+++ dot_config/fish/functions/glyphs.fish
@@ -1,3 +1,11 @@
 function glyphs --description '🎉 Print terminal-friendly glyphs and search them with fzf'
-    curl -s "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/glyphnames.json" | jq -r 'to_entries[] | "\(.key) \(.value.char)"' | tail +2 | fzf --no-mouse
+    curl -s "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/glyphnames.json" | jq -r '
+        to_entries[]
+        | select(.value.char != null)
+        | "\(.key)\t\(.value.char)"
+    ' | tail +2 | while read -l line
+        set -l parts (string split \t -- $line)
+        set -l cp (printf '%04x' "'$parts[2]")
+        echo "$parts[1] $parts[2] \\u$cp"
+    end | fzf --no-mouse
 end
