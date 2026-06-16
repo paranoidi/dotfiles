dca83a49c6fb4147a5cbcdece39cfa9d033d3572
paranoidi <marko.koivusalo@gmail.com>
2026-06-16 14:23:52 +0300

    feat: Scale keys helper for terminal size

--- dot_config/fish/functions/keys.fish
+++ dot_config/fish/functions/keys.fish
@@ -1,7 +1,7 @@
 function keys --description "🔑 Browse keyboard shortcuts with fzf"
     set -l keys_dir ~/.config/fish/keys
     set -l sections (string replace -r '.*/keys-' '' $keys_dir/keys-*)
-    set -l preview_cmd "cat $keys_dir/keys-{}"
+    set -l preview_cmd "$keys_dir/render $keys_dir/keys-{}"
 
     set -l selected (
         printf "%s\n" $sections |
