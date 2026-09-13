8ad671c5d9e32d4e31e188a073188b73fe6f272f
paranoidi <marko.koivusalo@gmail.com>
2026-09-14 01:16:36 +0300

    fix: EDITOR=hx as global, not universal
    Inherited env (e.g. a tmux server started when EDITOR=nvim) becomes a fish
    global and shadows universals, so set -Ux never took effect inside tmux.

--- dot_config/fish/conf.d/10-env.fish
+++ dot_config/fish/conf.d/10-env.fish
@@ -5,9 +5,12 @@ set -g man_standout -b 222226 ffff00
 set -Ux LANG en_US.UTF-8
 set -Ux LC_ALL en_US.UTF-8
 
-set -Ux EDITOR hx
-set -Ux VISUAL hx
-set -Ux GIT_EDITOR hx
+# -gx, not -Ux: a global overrides EDITOR inherited from a stale tmux server
+# or a parent shell; a universal is shadowed by it.
+set -gx EDITOR hx
+set -gx VISUAL hx
+set -gx GIT_EDITOR hx
+set -e -U EDITOR VISUAL GIT_EDITOR 2>/dev/null
 
 set -x EZA_THEME ~/.config/eza/theme.yml
 set -x BAT_THEME ansi
--- dot_config/fish/conf.d/99-editor.fish
+++
@@ -1,4 +0,0 @@
-# Last-resort override: some hosts/plugins leave EDITOR=nvim.
-set -Ux EDITOR hx
-set -Ux VISUAL hx
-set -Ux GIT_EDITOR hx
